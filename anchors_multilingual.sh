071dff6083195e922d63c05fea785b20bbc1de52
Hossain Shaikh Saadi <shaikh.saadi@tum.de>
2022-09-24 20:42:17 +0200

    initial commit

---
+++ anchors_multilingual.sh
@@ -0,0 +1,77 @@
+
+
+model=albert
+tgt_lang=en
+prefix=indic.cao
+lm_path=indic.bn-en.cao.pt
+
+batch_size=5
+max_sent=100
+max_len=256
+device=cuda
+src_lower_case=False
+tgt_lower_case=False
+src_model_path=ai4bharat/indic-bert
+tgt_model_path=ai4bharat/indic-bert
+total_sentences=200000000
+base_directory=/home/hsaadi/helper_folder
+single_layer_type=transformer
+single_layer_path=${base_directory}/saved_models/${prefix}.${single_layer_type}.bn-de-en.pt
+adapter_path=${base_directory}/saved_adapters/test_adapter/
+adapter_name=adapter_for_test
+
+:'
+src_lang=de
+lang_pair=${src_lang}-${tgt_lang}
+src_wiki=${src_lang}wiki-20210501-pages-articles-multistream_preprocessed_filtered.txt
+tgt_wiki=${tgt_lang}wiki-20210501-pages-articles-multistream_preprocessed_filtered.txt
+
+python generate_avg_anchors.py --input_dict /home/hsaadi/helper_folder/data/words/${src_lang}.txt \
+--lang ${src_lang} --input_file  /home/hsaadi/helper_folder/${src_wiki}  \
+--output_file  /home/hsaadi/helper_folder/embeddings/${prefix}.${lang_pair}.${src_lang} \
+--max_sent ${max_sent} --max_len ${max_len} --model ${model} \
+--model_path ${src_model_path} --temp_file temp/temp.txt \
+--lower_case False --total_sent ${total_sentences} --dimension 768 --batch ${batch_size} --device ${device} \
+--lm_path /home/hsaadi/helper_folder/saved_models/${lm_path}
+#--single_layer ${single_layer_path}
+#--adapter_path ${adapter_path} \
+#--adapter_name ${adapter_name} \
+#--single_layer ${single_layer_path}
+'
+:'
+previous_lang_pair=${lang_pair}
+src_lang=bn
+lang_pair=${src_lang}-${tgt_lang}
+src_wiki=${src_lang}wiki-20210501-pages-articles-multistream_preprocessed_filtered.txt
+
+python generate_avg_anchors.py --input_dict /home/hsaadi/helper_folder/data/words/${src_lang}.txt \
+--lang ${src_lang} --input_file  /home/hsaadi/helper_folder/${src_wiki}  \
+--output_file  /home/hsaadi/helper_folder/embeddings/${prefix}.${lang_pair}.${src_lang} \
+--max_sent ${max_sent} --max_len ${max_len} --model ${model} \
+--model_path ${src_model_path} --temp_file temp/temp.txt \
+--lower_case False --total_sent ${total_sentences} --dimension 768 --batch ${batch_size} --device ${device} \
+--lm_path /home/hsaadi/helper_folder/saved_models/${lm_path}
+#--single_layer ${single_layer_path}
+#--adapter_path ${adapter_path} \
+#--adapter_name ${adapter_name} \
+#--single_layer ${single_layer_path}
+'
+src_lang=bn
+tgt_wiki=${tgt_lang}wiki-20210501-pages-articles-multistream_preprocessed_filtered.txt
+lang_pair=${src_lang}-${tgt_lang}
+
+
+python generate_avg_anchors.py --input_dict /home/hsaadi/helper_folder/data/words/${tgt_lang}.txt \
+--lang ${tgt_lang} --input_file  /home/hsaadi/helper_folder/${tgt_wiki}  \
+--output_file  /home/hsaadi/helper_folder/embeddings/${prefix}.${lang_pair}.${tgt_lang} \
+--max_sent ${max_sent} --max_len ${max_len} --model ${model} \
+--model_path ${tgt_model_path} --temp_file temp/temp.txt \
+--lower_case False --total_sent ${total_sentences} --dimension 768 --batch ${batch_size} --device ${device} \
+--lm_path /home/hsaadi/helper_folder/saved_models/${lm_path}
+#--single_layer ${single_layer_path}
+
+#cp /home/hsaadi/helper_folder/embeddings/${prefix}.${single_layer_type}.${lang_pair}.${tgt_lang} \
+#/home/hsaadi/helper_folder/embeddings/${prefix}.${single_layer_type}.${previous_lang_pair}.${tgt_lang}
+
+#cp /home/hsaadi/helper_folder/embeddings/${prefix}.${lang_pair}.${tgt_lang} \
+#/home/hsaadi/helper_folder/embeddings/${prefix}.${previous_lang_pair}.${tgt_lang}
